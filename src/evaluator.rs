fcbc6b80abb86335e6a0f8983bd15707281b7890
ns <portification@gmail.com>
2017-05-19 09:59:16 +0000

    Implement very limited exponent evaluation

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,4 +1,7 @@
 use super::parser::{Expr, Op};
+use num::{ToPrimitive, BigRational};
+use num::rational::Ratio;
+use num::bigint::ToBigInt;
 
 pub fn evaluate(expression: Expr) -> Result<Expr, String> {
 	if let Expr::BinaryExpr(lhs, op, rhs) = expression {
@@ -14,7 +17,11 @@ pub fn evaluate(expression: Expr) -> Result<Expr, String> {
 					Op::Adjacent => return Ok(Expr::Number(lhs_i * rhs_i)),
 					Op::Divide => return Ok(Expr::Number(lhs_i / rhs_i)),
 					Op::Modulus => return Ok(Expr::Number(lhs_i % rhs_i)),
-					Op::Exponent => println!("NYI: a^b"),
+					Op::Exponent => {
+						if let Some(r) = ratio_power(lhs_i, rhs_i) {
+							return Ok(Expr::Number(r));
+						}
+					},
 					Op::Equals => return Ok(Expr::Boolean(lhs_i == rhs_i))
 				}
 			}
@@ -24,4 +31,27 @@ pub fn evaluate(expression: Expr) -> Result<Expr, String> {
 	}
 
 	Ok(expression)
+}
+
+fn ratio_power(lhs: &BigRational, rhs: &BigRational) -> Option<BigRational> {
+	if !rhs.is_integer() {
+		println!("Note: Non-integer exponents ({}) are not supported", rhs);
+		return None;
+	}
+
+	let power = rhs.numer().to_i32();
+	let numer = lhs.numer().to_isize();
+	let denom = lhs.denom().to_isize();
+
+	if let (Some(p), Some(n), Some(d)) = (power, numer, denom) {
+		let r = Ratio::new(n, d).pow(p);
+		let numer = r.numer().to_bigint();
+		let denom = r.denom().to_bigint();
+
+		if let (Some(n), Some(d)) = (numer, denom) {
+			return Some(BigRational::new(n, d));
+		}
+	}
+	
+	None
 }
