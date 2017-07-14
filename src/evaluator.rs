81adcdd6c790bdd363c92aec20f461988e56569f
ns <portification@gmail.com>
2017-07-14 12:29:42 +0200

    It keeps getting worse

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,5 +1,5 @@
 use super::parser::{Expr, Op};
-use num::{Zero, One, Signed, ToPrimitive, FromPrimitive, BigRational};
+use num::{pow, Zero, One, Signed, ToPrimitive, FromPrimitive, BigRational};
 use num::rational::Ratio;
 use num::bigint::ToBigInt;
 
@@ -25,16 +25,29 @@ fn normalize(expr: &Expr) -> Expr {
             let mut rhs = normalize(rhs);
 
             if op == Op::Adjacent {
+                // TODO: Turn (function a) into Application(function, a)
+                // TODO: Turn (a unit) into Measure(a, unit)
                 op = Op::Multiply;
             }
 
             if op == Op::Subtract {
                 // (a - b) => (a + (-1 * b))
-                op = Op::Add;
-                rhs = Expr::BinaryExpr(
-                    Box::new(Expr::Number(neg)),
-                    Op::Multiply,
-                    Box::new(rhs));
+                /*let zero = if let Expr::Number(ref a) = lhs {
+                    a.is_zero()
+                } else {
+                    false
+                };
+
+                if zero {
+                    lhs = Expr::Number(neg);
+                    op = Op::Multiply;
+                } else {*/
+                    op = Op::Add;
+                    rhs = Expr::BinaryExpr(
+                        Box::new(Expr::Number(neg)),
+                        Op::Multiply,
+                        Box::new(rhs));
+                //}
             } else if op == Op::Divide {
                 // (a / b) => (a * (b ^ -1))
                 op = Op::Multiply;
@@ -78,16 +91,73 @@ fn apply_associative(op: Op, lhs: Expr, rhs: Expr) -> (Expr, Expr) {
 // Assumes that `expr` has already been normalized via `normalize()`
 fn simplify(expr: &Expr) -> Expr {
     let new_expr = simplify_inner(expr);
-    println!("simplify {} => {}", expr, &new_expr);
+
+    if expr != &new_expr  {
+        // println!("simplify {} => {}", expr, &new_expr);
+    }
+
     new_expr
 }
 
 fn simplify_inner(expr: &Expr) -> Expr {
-    match expr {
-        &Expr::BinaryExpr(_, Op::Add, _) => simplify_add(expr),
-        &Expr::BinaryExpr(_, Op::Multiply, _) => simplify_multiply(expr),
+    match *expr {
+        Expr::BinaryExpr(_, Op::Add, _) => simplify_add(expr),
+        Expr::BinaryExpr(_, Op::Multiply, _) => simplify_multiply(expr),
+        Expr::BinaryExpr(ref a, Op::Exponent, ref b) => {
+            let a = simplify(a);
+            let b = simplify(b);
+
+            if let Expr::Number(ref a) = a {
+                if let Expr::Number(ref b) = b {
+                    if let Some(n) = real_power(a, b) {
+                        return Expr::Number(n);
+                    }
+                }
+            }
+
+            Expr::BinaryExpr(Box::new(a), Op::Exponent, Box::new(b))
+        },
         _ => expr.clone()
     }
+}
+
+fn real_power(a: &BigRational, b: &BigRational) -> Option<BigRational> {
+    if !b.is_integer() {
+        return None;
+    }
+
+    // a ^ -1
+    if b.numer() == &-1.to_bigint().unwrap() {
+        return Some(BigRational::new(
+            a.denom().clone(),
+            a.numer().clone()));
+    }
+
+    // ( 1/1) ^ any
+    // (-1/1) ^ any
+    if a.is_integer() && (a.numer().abs() == 1.to_bigint().unwrap()) {
+        return Some(a.clone());
+    }
+
+    // (a/1) ^ usize
+    if a.denom() == &1.to_bigint().unwrap() {
+        if let Some(exp) = b.numer().to_usize() {
+            let base = a.numer().clone();
+            return Some(BigRational::new(pow(base, exp), 1.to_bigint().unwrap()));
+        }
+    }
+
+    // (isize/isize) ^ i32
+    if let (Some(p), Some(n), Some(d)) = (b.numer().to_i32(),
+                                          a.numer().to_isize(),
+                                          b.denom().to_isize()) {
+        let r = Ratio::new(n, d).pow(p);
+        let n = r.numer().to_bigint().unwrap();
+        let d = r.denom().to_bigint().unwrap();
+        return Some(BigRational::new(n, d));
+    }
+
+    None
 }
 
 fn simplify_add(expr: &Expr) -> Expr {
@@ -184,13 +254,12 @@ fn simplify_multiply(expr: &Expr) -> Expr {
         if let Expr::Number(ref n) = terms[i].1 {
             coeff = &coeff * n;
             new.0 = BigRational::zero();
-        } else if let Expr::BinaryExpr(_, Op::Multiply, _) = terms[i].1 {
-            panic!("multiply");
-        } else if let Expr::BinaryExpr(_, Op::Exponent, _) = terms[i].1 {
-            panic!("exponent");
+        } else if let Expr::BinaryExpr(ref a, Op::Exponent, ref b) = terms[i].1 {
+            if let Expr::Number(ref n) = **b {
+                new.2 = n.clone();
+                new.1 = (**a).clone();
+            }
         }
-        // shouldn't be a need to handle multiply, but do so here
-        // handle exponent here
 
         terms[i] = new;
     }
